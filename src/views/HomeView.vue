6c926347c5f7f6f975fc00a5086ae2715bdf2c4a
张春龙 <41391477@qq.com>
2024-01-24 20:44:51 +0800

    nn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,6 +30,18 @@
 				</el-card>
 			</el-col>
 		</el-row>
+		<el-row style="margin-top: 15px;">
+			<el-col :span="24">
+				<el-card class="box-card" shadow="hover">
+					<template #header>
+						<div class="card-header">
+							<span>其他内容</span>
+						</div>
+					</template>
+					<div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+				</el-card>
+			</el-col>
+		</el-row>
 	</div>
 </template>
   
