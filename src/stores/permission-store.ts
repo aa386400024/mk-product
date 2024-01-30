24994a811dda591b44f9de7b95b30effa9034048
张春龙 <41391477@qq.com>
2024-01-30 17:43:54 +0800

    aa

--- src/stores/permission-store.ts
+++ src/stores/permission-store.ts
@@ -3,9 +3,8 @@ import type { RouteRecordRaw } from 'vue-router';
 import { constantRoutes, addAsyncRoutes } from '@/router';
 import { fetchRouterList } from '@/api/router/list';
 
-const modules = import.meta.glob('../../views/**/**.vue');
+const modules = import.meta.glob('../views/**/**.vue');
 const Layout = () => import('@/layouts/MainLayout.vue');
-
 // 判断是否有权限
 const hasPermission = (roles: string[], route: RouteRecordRaw) => {
     if (route.meta && route.meta.roles) {
@@ -25,20 +24,22 @@ const filterAsyncRoutes = (routes: RouteRecordRaw[], roles: string[]) => {
     const asyncRoutes: RouteRecordRaw[] = [];
 
     routes.forEach(route => {
+        console.log(route, 'route')
         const tmpRoute = { ...route };
         if (!route.name) {
             tmpRoute.name = route.path;
         }
         if (hasPermission(roles, tmpRoute)) {
             console.log(tmpRoute, 'tmpRoute')
-            if (tmpRoute.component?.toString() === 'Layout') {
+            if (tmpRoute.component?.toString() === 'MainLayout') {
                 tmpRoute.component = Layout;
             } else {
-                const component = modules[`../../views/${tmpRoute.component}.vue`];
+                const component = modules[`../views/${tmpRoute.component}.vue`];
+                console.log(component, 'component')
                 if (component) {
                     tmpRoute.component = component;
                 } else {
-                    tmpRoute.component = modules['../../views/error-page/404.vue'];
+                    tmpRoute.component = modules['../views/error-page/404.vue'];
                 }
             }
 
@@ -67,7 +68,19 @@ export const usePermissionStore = defineStore({
         },
         async generateRoutes(roles: string[]) {
             try {
-                const { data: asyncRoutes } = await fetchRouterList({});
+                // const { data: asyncRoutes } = await fetchRouterList({});
+                const asyncRoutes = [
+                    {
+                        path: '/',
+                        component: 'MainLayout', // 由于这是硬编码数据，我们只能用字符串来表示组件
+                        children: [
+                            { path: '', name: 'home', component: 'HomeView' },
+                            { path: 'about', name: 'about', component: 'AboutView' }
+                        ]
+                    }
+                    // 可以根据需要添加更多路由
+                ];
+                
                 const accessedRoutes = filterAsyncRoutes(asyncRoutes, roles);
                 this.setRoutes(accessedRoutes);
                 return accessedRoutes;
--- src/stores/user-store.ts
+++ src/stores/user-store.ts
@@ -30,7 +30,7 @@ export const useUserStore = defineStore('user', {
             // 假设这是从后端获取的用户信息
             const userInfoFromApi = {
                 username: 'admin',
-                roles: ['admin', 'editor']
+                rolesss: ['admin', 'editor']
             }
             this.userinfo = userInfoFromApi // 设置用户信息
             router.push({ name: 'home' })
@@ -38,6 +38,11 @@ export const useUserStore = defineStore('user', {
 
         getUserInfo() {
             // 返回用户信息
+            const aa = {
+                username: 'admin',
+                roles: ['admin', 'editor']
+            }
+            this.userinfo = aa
             return this.userinfo
         },
 
