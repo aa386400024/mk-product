74a25fd49f8a7e1d4b0a6c389cdcb25ff92bf849
张春龙 <41391477@qq.com>
2024-02-19 09:08:53 +0800

    ceshi

--- src/components/dropdown/MyDropdown.vue
+++ src/components/dropdown/MyDropdown.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits } from 'vue';
+import { ref } from 'vue';
 import { clickOutside as vClickOutside } from "v-click-outside-vue3";
 
 interface Option {
