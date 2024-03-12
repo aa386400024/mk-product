a114516dd01476e2e4bc1ed4cbd5e775736c2066
张春龙 <41391477@qq.com>
2024-03-12 18:10:26 +0800

    kj

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,17 @@ import { setupQiankun } from './plugins/qiankun';
 import './permission'
 import { SvgIcon } from './components/icons';
 import { WebSocketPlugin } from './plugins/websocket-plugin';
-import * as ChatUI from '@springdragon/chat-ui';
-// import '@springdragon/chat-ui/dist/style.css';
-import '/node_modules/@springdragon/chat-ui/dist/style.css';
+import ChatUI from '@springdragon/chat-ui';
+// import '@springdragon/chat-ui/lib/style.css';
+
+import '/node_modules/@springdragon/chat-ui/lib/style.css';
 
 const app = createApp(App)
 
 // 全局注册 SvgIcon 组件
 app.component('SvgIcon', SvgIcon);
 
-app.use(ChatUI.install, { defaultTheme: 'dark' });
+app.use(ChatUI, { defaultTheme: 'dark' });
 app.use(pinia)
 app.use(router)
 app.use(new WebSocketPlugin(), { url: import.meta.env.VITE_SOCKET_IO_URL });
